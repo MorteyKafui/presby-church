e7dce8310d930aaf50aa351f4967f0f2e5701385
Kafui Mortey <morteykafui@gmail.com>
2023-12-24 17:28:29 +0000

    add missions

--- components/Aim.tsx
+++ components/Aim.tsx
@@ -1,3 +1,4 @@
+import { ScrollText } from "lucide-react";
 import Image from "next/image";
 
 const Aim = () => {
@@ -32,13 +33,36 @@ const Aim = () => {
           <span className="bg-red-800 font-bold text-xl shadow-2xl text-white px-10 py-2 ">
             Mission
           </span>
-          <p className="my-10 md:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-            reiciendis possimus quae sapiente eum commodi. Laboriosam explicabo
-            animi fuga at! Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Aliquid reiciendis possimus quae sapiente eum commodi.
-            Laboriosam explicabo animi fuga at!
-          </p>
+          <ul className="my-10 md:text-xl text-blue-900 font-bold flex flex-col gap-8">
+            <li className="flex items-center gap-5">
+              <ScrollText size={30} className="text-red-800" />
+              <p className="flex-1">Mobilizing the entire church for prayer</p>
+            </li>
+            <li className="flex items-center gap-5">
+              <ScrollText size={30} className="text-red-800" />
+              <p className="flex-1">
+                Improving church growth through evangelism and nurture
+              </p>
+            </li>
+            <li className="flex items-center gap-5">
+              <ScrollText size={30} className="text-red-800" />
+              <p className="flex-1">
+                Attaining self-sufficiency through effective resource
+                mobilization
+              </p>
+            </li>
+            <li className="flex items-center gap-5">
+              <ScrollText size={30} className="text-red-800" />
+              <p className="flex-1">
+                Promoting socio-economic development development through
+                advocacy and effective delivery of social service
+              </p>
+            </li>
+            <li className="flex items-center gap-5">
+              <ScrollText size={30} className="text-red-800" />
+              <p className="flex-1">Upholding the reformed tradition</p>
+            </li>
+          </ul>
         </div>
 
         <Image
